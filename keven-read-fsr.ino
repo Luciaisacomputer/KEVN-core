aea1708744319da18531fce79bbd46f1b0e8d176
Luke Pettway <luke.pettway@gmail.com>
2017-06-02 13:34:33 -0400

    Update keven-read-fsr.ino
    Updated values

--- keven-read-fsr.ino
+++ keven-read-fsr.ino
@@ -19,14 +19,11 @@ void loop(void) {
   Serial.print("Analog reading = ");
   Serial.print(fsrReading);     // the raw analog reading
  
-  // We'll have a few threshholds, qualitatively determined
-  if (fsrReading < 10) {
-    Serial.println(" - No pressure");
-  } else if (fsrReading < 200) {
-    Serial.println(" - Light touch");
-  } else if (fsrReading < 500) {
+  if (fsrReading < 200) {
+    Serial.println(" - No touch");
+  } else if (fsrReading < 400) {
     Serial.println(" - Light squeeze");
-  } else if (fsrReading < 800) {
+  } else if (fsrReading < 600) {
     Serial.println(" - Medium squeeze");
   } else {
     Serial.println(" - Big squeeze");
